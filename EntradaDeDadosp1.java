04f6bffd49c6528c390c92b73c1f8f578faea1ff
Moost <111387631+Moost999@users.noreply.github.com>
2025-01-30 18:51:10 -0300

    Create EntradaDeDadosp1.java

---
+++ EntradaDeDadosp1.java
@@ -0,0 +1,36 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+
+    Scanner sc = new Scanner(System.in);
+
+    String s1,s2,s3;
+    int x;
+    
+
+    System.out.println("Dados digitados:");
+
+    x = sc.nextInt();
+    sc.nextLine(); // BUFFER PRA LIMPAR O CACHE DO NEXTLINE();
+    s1 = sc.nextLine();
+    s2 = sc.nextLine();
+    s3 = sc.nextLine(); 
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+    System.out.println(x);
+
+    sc.close();
+  
+  }
+
+}
